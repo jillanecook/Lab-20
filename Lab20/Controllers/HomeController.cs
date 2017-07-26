9a9e186e3dc9abe057b0842b065e5bd876cb7499
Jillane Cook <cookjillane@gmail.com>
2017-07-26 08:55:00 -0400

    Initial Commit

---
+++ Lab20/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab20.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Fresh Beans. Fresh Start.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "We want to hear from you.";
+
+            return View();
+        }
+        public ActionResult Registration()
+        {
+            ViewBag.Message = "Register Here";
+
+            return View();
+        }
+        public ActionResult ValidateForm()
+        {
+            ViewBag.Message = "Register Here";
+
+            return View();
+        }
+    }
+}
---
+++ Lab20/Controllers/Model.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+//using System.Data.Entity;
+
+//namespace Lab20;
+////{
+//    public class Model
+//    {
+//        [Required]
+//        [Key]
+//        public string FirstName { get; set; }
+
+//        [Required]
+//        public string Email { get; set; }
+//    }
+
+//    public class DBRegisterContext : DbContext
+//    {
+//        public DbSet<Register> Entries { get; set; }
+
+//        public System.Data.Entity.DbSet<Lab20.Models.Item> Items { get; set; }
+//    }
+//}
+    
